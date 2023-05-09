f811c0fa11ebea1f53542ac225a7d3a4bf8ec2cd
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2023-05-09 15:06:39 -0400

    Operador Ternario y operadores logicos

--- condicionales.js
+++ condicionales.js
@@ -47,3 +47,70 @@ if (opcion === 1){
 } else {
     console.log('Opcion Invalida');
 }
+
+
+switch(option){
+    case 1:
+        console.log('Opcion 1');
+        break;
+    case 2:
+        console.log('Opcion 2');
+        break;
+
+    case 3:
+    case 4:
+    case 5:
+        console.log('La opcion es 3, 4, o 5');
+        break;
+    default:
+        console.log('Opcion invalida');
+        break;
+}
+
+console.log("Hola otra vez");
+
+/* Operadores Logicos (and, or, not) => (&&, ||, !) */
+/* 
+// AND (&&)
+true && true => true
+true && false => false
+false && false => false
+
+// OR (||)
+true || true => true
+true || false => true
+false || false => false
+
+// NOT (!)
+!true && !true => false
+!true && !false => false
+!false && !false => true
+
+// NOT (!)
+!true || !true => false
+!true || !false => true
+!false || !false => true
+
+*/
+
+
+let a = 5;
+let b = 6;
+let c = 8;
+
+if (a > b && a > c){
+    console.log('A');
+}
+
+if (a > b || a > c || b < c){
+    console.log('A');
+}
+
+
+if(!(a > b) && !(a > c)){
+    console.log('A no es mayor que b y c');
+}
+
+let mayor = edad >= 18 ? true : false;
+let hora = 9
+let saludo = hora >= 8 && hora <= 12 ? "Buenos dias" : "Buenas Tardes"
